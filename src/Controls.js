6e9dac2ea149aad9103265656b90ac347570324f
MahaaNasir <mahanasir25392@gmail.com>
2024-10-06 22:57:15 -0400

    updated

--- src/Controls.js
+++ src/Controls.js
@@ -5,3 +5,18 @@ controls.enableDamping = true;
 controls.dampingFactor = 0.25;
 controls.screenSpacePanning = false;
 controls.maxPolarAngle = Math.PI / 2;
+// server.js (Backend)
+const express = require('express');
+const app = express();
+
+app.use(express.static('public', {
+  setHeaders: (res, path) => {
+    if (path.endsWith('.css')) {
+      res.setHeader('Content-Type', 'text/css');
+    } else if (path.endsWith('.js')) {
+      res.setHeader('Content-Type', 'application/javascript');
+    }
+  }
+}));
+
+app.listen(3000, () => console.log('Server running on port 3000'));
