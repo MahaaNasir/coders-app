1610f44777d77c985b2470cfc228d6a7517c5068
MahaaNasir <mahanasir25392@gmail.com>
2024-10-06 22:49:56 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import Orrery from './Orrery';
+const express = require('express');
+const app = express();
+
+app.use(express.static('public', {
+  setHeaders: (res, path) => {
+    if (path.endsWith('.css')) {
+      res.setHeader('Content-Type', 'text/css');
+    } else if (path.endsWith('.js')) {
+      res.setHeader('Content-Type', 'application/javascript');
+    }
+  }
+}));
+
+app.listen(3000, () => console.log('Server running on port 3000'));
 
 const App = () => {
   return (
